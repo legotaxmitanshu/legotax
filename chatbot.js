3b1ffe8441353c2b181141b93178b3410228274e
legotaxmitanshu <mitanshusharma031@gmail.com>
2025-12-30 19:08:38 +0530

    Create chatbot.js

---
+++ chatbot.js
@@ -0,0 +1,15 @@
+// chatbot.js
+(function() {
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.async = true;
+  script.src = 'https://cdn.openwidget.com/openwidget.js';
+  script.onload = function() {
+    window.__ow = window.__ow || {};
+    window.__ow.organizationId = "464cb5c2-8365-4bad-be15-3682987750b5";
+    window.__ow.template_id = "b39785b4-f596-432e-8021-c54169945c34";
+    window.__ow.integration_name = "manual_settings";
+    window.__ow.product_name = "chatbot";   
+  };
+  document.body.appendChild(script);
+})();
